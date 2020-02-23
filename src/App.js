448c77e46be99ace6cc9ac00af39201025f141a0
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-02-23 20:49:10 +0400

    change giphy

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 // import SendboxButton from './Component/Button/SendboxButton';
 // import SendboxTab from './Component/Tabs/SendboxTab'
 // import SendboxButtonGroup from './Component/Button/SendboxButtonGroup'
-import Picker from './Component/Giphy/Picker';
-import Giphy from './Component/Giphy/Gipgy';
-// import IndexGiphy from './Component/GiphyNew/IndexGiphy';
+// import Picker from './Component/Giphy/Picker';
+// import Giphy from './Component/Giphy/Gipgy';
+import IndexGiphy from './Component/GiphyNew/IndexGiphy';
 // import Graph_D3 from './Component/Graph-D3/Graph_D3';
 // import SendBoxChart from './Component/Chart/SendBoxChart'; 
 // import Babelon from './Component/Babelon/Babelon';
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <> 
-      <Giphy /> 
+      <IndexGiphy /> 
     </>
   )
 }
