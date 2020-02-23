a92a3420b5f6609005ace498cd0ad0cf7a29c246
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-02-23 20:22:08 +0400

    change giphy

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 // import SendboxButton from './Component/Button/SendboxButton';
 // import SendboxTab from './Component/Tabs/SendboxTab'
 // import SendboxButtonGroup from './Component/Button/SendboxButtonGroup'
-// import Picker from './Component/Giphy/Picker';
-// import Giphy from './Component/Giphy/Gipgy';
+import Picker from './Component/Giphy/Picker';
+import Giphy from './Component/Giphy/Gipgy';
+// import IndexGiphy from './Component/GiphyNew/IndexGiphy';
 // import Graph_D3 from './Component/Graph-D3/Graph_D3';
-import SendBoxChart from './Component/Chart/SendBoxChart'; 
+// import SendBoxChart from './Component/Chart/SendBoxChart'; 
 // import Babelon from './Component/Babelon/Babelon';
 // import Responsive from './Component/Responsive/Responsive';
 // import Subscribe from './Component/NewsSubscribe/Subscribe';
-// import IndexGiphy from './Component/GiphyNew/IndexGiphy';
 // import Form from './Component/Form/Form';
 // import Ui from './Component/Form/Ui';
 // import State from './Component/Hooks/useState';
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <> 
-      <SendBoxChart /> 
+      <Giphy /> 
     </>
   )
 }
--- src/Component/GiphyNew/IndexGiphy.js
+++ src/Component/GiphyNew/IndexGiphy.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import ReactGiphySearchbox from 'react-giphy-searchbox'
 
-const API_KEY = 't0WziV5gGejyE99IJyPa0oZmrFVS7iJr'
+const API_KEY = 't0WziV5gGejyE99IJyPa0oZmrFVS7iJr';
 
 const IndexGiphy = () => {
     const [url, setUrl] = useState('');
