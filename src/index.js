32f80f077f65ec3c94558a90b895f6a38a256aa2
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-07-24 23:31:20 +0400

    add: react query  base-comp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import EntryPoint from './Component';
-import { Hook } from './Hooks/index.js'
+// import EntryPoint from './Components';
+import { Hook } from './Hooks/index.js';
+import { Query } from './Query/index.js';
 
 ReactDOM.render(<Hook />, document.getElementById('root'));
